717b40d334009b18b5baecfa5b6b9b2ca1dcf24e
Adepoju Oluwatobi <kunletobi4@gmail.com>
2024-08-11 21:18:52 +0100

    send file index.html

--- myApp.js
+++ myApp.js
@@ -3,8 +3,10 @@ let app = express();
 
 console.log("Hello World")
 
+const absolutePath = __dirname + '/views/index.html'
+
 app.get('/', function(req, res){
-    res.send('Hello Express')
+    res.sendFile(absolutePath)
 })
 
 
